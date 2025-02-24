2e83abb62e97377c3ef38f4c9d2715ff6ed5e4e4
24LP9763 24LP9763 <24LP9763@administrators-iMac-3.local>
2025-02-24 18:08:08 +0800

    memo

--- app/page.js
+++ app/page.js
@@ -1,95 +1,45 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [match, setMatch] = useState();
+  const [cardValue, setCardValue] = useState([
+    { value: 1, isShow: false },
+    { value: 2, isShow: false },
+    { value: 3, isShow: false },
+    { value: 4, isShow: false },
+    { value: 5, isShow: false },
+    { value: 6, isShow: false },
+    { value: 7, isShow: false },
+    { value: 8, isShow: false },
+    { value: 9, isShow: false },
+    { value: 10, isShow: false },
+    { value: 11, isShow: false },
+    { value: 12, isShow: false },
+  ]);
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.container}>
+        <div className={styles.board}>
+          {cardValue.map((item, i) => {
+            return (
+              <div className={styles.cards} key={i}>
+                {item.isShow == true ? item.value : ""}
+              </div>
+            );
+          })}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.spec}>
+          <div className={styles.mistakes}></div>
+          <div className={styles.moves}></div>
+          <div className={styles.time}></div>
+        </div>
+      </div>
     </div>
   );
 }
+
+// ergedeg togloom
+// darahad ungu ni uurchlugduud gardag bh
